e88c84db9541dae1f3ce6f28e13b89fba7a2c6e2
Matthew Carter <m@ahungry.com>
2020-04-04 23:39:33 -0400

    Create the toy impl for the janet testing

--- Examples/janet/trivial-math.c
+++ Examples/janet/trivial-math.c
@@ -1,4 +1,11 @@
- /* File : trivial-math.c */
+/* File : trivial-math.c */
+#include <stdlib.h>
+#include <stdio.h>
+
+struct point {
+  int x;
+  int y;
+};
 
 int sum_nums (int a, int b) {
   return a + b;
@@ -6,4 +13,33 @@ int sum_nums (int a, int b) {
 
 int diff_nums (int x, int y) {
   return x - y;
+}
+
+struct point *
+make_point (int x, int y)
+{
+  struct point *p = malloc (sizeof (struct point));
+
+  p->x = x;
+  p->y = y;
+
+  return p;
+}
+
+int
+sum_point (struct point *p)
+{
+  return p->x + p->y;
+}
+
+int
+main (int argc, char *argv[])
+{
+  struct point *p1 = make_point (3, 4);
+
+  printf ("Sum is: %d", sum_point (p1));
+
+  free (p1);
+
+  return 0;
 }
--- Examples/janet/trivial-math.i
+++ Examples/janet/trivial-math.i
@@ -5,13 +5,14 @@
   extern int sum_nums (int a, int b);
   extern int diff_nums (int x, int y);
 
-  // extern struct point { int x, int y };
+  // FIXME: Atm the output doesn't create the helpers for this...
+  // extern struct point { int x; int y; };
   extern struct point * make_point (int x, int y);
   extern int sum_point (struct point *p);
   %}
 
 extern int sum_nums (int a, int b);
 extern int diff_nums (int x, int y);
-// extern struct point { int x, int y };
+// struct point { int x; int y; };
 extern struct point * make_point (int x, int y);
 extern int sum_point (struct point *p);
